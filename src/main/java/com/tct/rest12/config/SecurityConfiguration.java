6c8f0c99f2bcbd8cc83b46f059abbc925e6e4906
kejdi <kejdielezi1@gmail.com>
2023-06-15 20:17:49 +0200

    a

--- src/main/java/com/tct/rest12/config/SecurityConfiguration.java
+++ src/main/java/com/tct/rest12/config/SecurityConfiguration.java
@@ -13,6 +13,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
         auth.inMemoryAuthentication()
                 .withUser("kejdi")
                 .password("{noop}password")
